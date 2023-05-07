b59e940ec8373d47bed5d59d752745bcffec2f50
ddswoosh <ddswoosh@gmail.com>
2023-05-07 16:42:33 -0700

    Update UserDB.py
    This is just the foundation of the authentication, this does not represent the actual syntax. Just a rough draft.

--- UserDB.py
+++ UserDB.py
@@ -5,36 +5,50 @@ class Data:
     def __init__(self):
         
         self.head = None
-        self.current_node = None
-        self.previous_node = None
+        self.current_node = self.head
+        self.next = None
 
-        self.username = pyscript.js(api)
-        self.password = pyscript.js(api)
-        self.dict = {}
+        self.username = #input(pyscript.js)
+        self.password = #input(pyscript.js)
         self.auth = False
     
     def firstNode(self):
-        if self.login > "":
+        if self.head == None
             self.head = int(1)
- 
+            #sql insert self.head into column 1
             nextNode()
              
-    def nextNode(self):
-         self.login = input(login_popup.pyscript)
- 
-         if self.login in maindb:
-             return current_id
- 
+    def secondNode(self):
+        if self.current_node == self.head
+            self.current_node = self.head + int(1)
+            #sql inset self.current_node into column 1
+            currentNode()
+               
+    def currentNode(self):
+        self.current_node = self.current_node + int(1)
+        #sql insert self.current_node into column 1
         
-        
-# if self.login in sqlite3(db.sql)
-# once auth reutrn current id
-# id stores class variables lifetime 
-# if id is in db, call variables from relational db where id is foreign key
- 
-# if no id , create id current node + 1 and run previous script
-# use previous node to credit current node, current_node = previous_node+1
-# for first id in db, create self.head = 1
-# id is always stores primary key in auth(db)
-# id is foerign key in table store class var such as credit                
-    
+    def auth(self):
+        if self.username in sqldb
+        {
+            SELECT username
+            FROM sqldb.table
+            WHERE username == username 
+        }
+            if self.password in sqldb
+            {
+                SELECT password
+                FROM sqldb.table
+                WHERE password == password
+            }
+                return sqldb
+                {
+                    SELECT id
+                    FROM sqldb.table
+                    WHERE self.username == username AND self.password == password
+                }
+        else:
+            #sql insert self.username into column 2
+            #sql inset self.password into column 3
+            firstNode()
+
