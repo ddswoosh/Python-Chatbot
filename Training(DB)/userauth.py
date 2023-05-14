94a47fba5307bf7022c1837231a995a03ca1adfb
ddswoosh <ddswoosh@gmail.com>
2023-05-13 22:56:06 -0700

    Update userauth.py

--- Training(DB)/userauth.py
+++ Training(DB)/userauth.py
@@ -1,1 +1,54 @@
+import sqlite3
+
+class Data:
+    
+    def __init__(self):
+        
+        self.head = None
+        self.current_node = self.head
+        self.next = None
+
+        self.username = #input(pyscript.js)
+        self.password = #input(pyscript.js)
+        self.auth = False
+    
+    def firstNode(self):
+        if self.head == None
+            self.head = int(1)
+            #sql insert self.head into column 1
+            nextNode()
+             
+    def secondNode(self):
+        if self.current_node == self.head
+            self.current_node = self.head + int(1)
+            #sql inset self.current_node into column 1
+            currentNode()
+               
+    def currentNode(self):
+        self.current_node = self.current_node + int(1)
+        #sql insert self.current_node into column 1
+        
+    def auth(self):
+        if self.username in sqldb
+        {
+            SELECT username
+            FROM sqldb.table
+            WHERE username == username 
+        }
+            if self.password in sqldb
+            {
+                SELECT password
+                FROM sqldb.table
+                WHERE password == password
+            }
+                return sqldb
+                {
+                    SELECT id
+                    FROM sqldb.table
+                    WHERE self.username == username AND self.password == password
+                }
+        else:
+            #sql insert self.username into column 2
+            #sql inset self.password into column 3
+            firstNode()
 
